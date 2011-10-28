5e726f15f6e179d4a2f4a62eebc05ff7d7fef80b
Andrew Radev <andrey.radev@gmail.com>
2011-10-28 22:53:09 +0300

    Some fixes and more steps

--- lib/cucumber/vimscript.rb
+++ lib/cucumber/vimscript.rb
@@ -1,4 +1,5 @@
 require 'tmpdir'
+require 'tempfile'
 require 'fileutils'
 require 'vimrunner/runner'
 
@@ -17,8 +18,21 @@ Given /^Vim is running$/ do
   @vim = Vimrunner::Runner.start_gvim
 end
 
-When /^I start editing a file named "([^"]*)"$/ do |filename|
+Given /^I'm editing a file named "([^"]*)"$/ do |filename|
   @vim.edit filename
+end
+
+Given /^I'm editing a file named "([^"]*)" with the following contents:$/ do |filename, text|
+  File.open(filename, 'w') { |f| f.write(text) }
+  @vim.edit filename
+end
+
+Given /^"([^"]*)" is set$/ do |boolean_setting|
+  @vim.set boolean_setting
+end
+
+Given /^"([^"]*)" is set to "([^"]*)"$/ do |setting, value|
+  @vim.set setting, value
 end
 
 When /^I insert the text "([^"]*)"$/ do |text|
@@ -27,9 +41,19 @@ end
 
 When /^I save$/ do
   @vim.write
+  @vim.wait_until_ready
 end
 
 Then /^the file "([^"]*)" should contain "([^"]*)"$/ do |filename, text|
   File.exists?(filename).should be_true
   File.read(filename).should include text
+end
+
+Then /^the file "([^"]*)" should contain the following text:$/ do |filename, text|
+  File.exists?(filename).should be_true
+  File.read(filename).should include text
+end
+
+Given /^the cursor is positioned on "([^"]*)"$/ do |text|
+  @vim.search text
 end
